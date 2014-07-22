ac8b8cb263aaca0e80c7b94fe10f00ea1a7d238d
kengood1 <kengoodman25@gmail.com>
2014-07-21 17:57:11 -0700

    Clean up. Created sub method

--- Main.aspx.vb
+++ Main.aspx.vb
@@ -3,32 +3,43 @@
 Public Class Main
   Inherits System.Web.UI.Page
   Public DList As String = ""
+
+  ''' <summary>
+  ''' Page_Load
+  ''' </summary>
+  ''' <param name="sender"></param>
+  ''' <param name="e"></param>
+  ''' <remarks></remarks>
   Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    'Gets list of clients
+    GetClients()
+
+  End Sub
+
+  ''' <summary>
+  ''' Gets the list of clients in ClientUpdate table
+  ''' </summary>
+  ''' <remarks></remarks>
+  Private Sub GetClients()
+
     Dim db As dbUtil 'access to db functions
     Dim rsData As SqlDataReader
 
     db = New dbUtil()
-    DList = ""
-    'rsData = db.GetDataReader("select Top 5 * from DoctorList")
     rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact FROM ClientUpdate WITH (NOLOCK)")
-
-    'If rsData.HasRows Then
-    '  Do While rsData.Read()
-    '    DList = DList & rsData(1)
-    '  Loop
-    'End If
 
     Dim row As HtmlTableRow
     Dim cell As HtmlTableCell
     Dim a As HtmlAnchor
     Do While rsData.Read()
       row = New HtmlTableRow()
-      cell = New HtmlTableCell 'With {.InnerText = rsData(0)}
+      cell = New HtmlTableCell
       a = New HtmlAnchor With {.HRef = "#"}
       a.InnerText = rsData(0)
       cell.Controls.Add(a)
       row.Controls.Add(cell)
-      cell = New HtmlTableCell 'With {.InnerText = rsData(1)}
+      cell = New HtmlTableCell
       a = New HtmlAnchor With {.HRef = "#"}
       a.InnerText = rsData(1)
       cell.Controls.Add(a)
