a54835ce2fe183589de53d7b87f0d7edffdcb45e
Harvey Goodman <hgoodman@cgc-intl.com>
2014-08-04 13:52:44 -0700

    xx

--- Messages.aspx.vb
+++ Messages.aspx.vb
@@ -6,150 +6,150 @@ Public Class Messages
   Inherits System.Web.UI.Page
 
   Protected clientGreeting As String = ""
-  Protected cid As String = ""
-  Public fromNames As String = ""
+    Protected cid As String = ""
+    Public fromNames As String = ""
 
-  ''' <summary>
-  ''' 
-  ''' </summary>
-  ''' <param name="sender"></param>
-  ''' <param name="e"></param>
-  ''' <remarks></remarks>
-  Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-    Dim successed As Boolean
-    Dim clientId As String
-    Dim msgId As Integer
+        Dim successed As Boolean
+        Dim clientId As String
+        Dim msgId As Integer
 
-    If Not (Page.IsPostBack) Then
+        If Not (Page.IsPostBack) Then
 
-      ' Get Lookup Data
-      GetMessageLookup()
+            ' Get Lookup Data
+            GetMessageLookup()
 
-      msgId = Convert.ToInt32(Request.QueryString.Get("MsgId"))
-      clientId = Request.QueryString.Get("ClientId").ToString()
+            msgId = Convert.ToInt32(Request.QueryString.Get("MsgId"))
+            clientId = Request.QueryString.Get("ClientId").ToString()
 
-      If Not String.IsNullOrEmpty(clientId) Then
-        cid = clientId
-        successed = GetClient(clientId)
+            If Not String.IsNullOrEmpty(clientId) Then
+                cid = clientId
+                successed = GetClient(clientId)
 
-        If msgId > 0 Then
-          'Message Exists
+                If msgId > 0 Then
+                    'Message Exists
 
+
+                End If
+            End If
 
         End If
-      End If
 
-    End If
+    End Sub
 
-  End Sub
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub GetMessageLookup()
 
-  ''' <summary>
-  ''' 
-  ''' </summary>
-  ''' <remarks></remarks>
-  Private Sub GetMessageLookup()
+        Dim db As dbUtil 'access to db functions
+        Dim rsData As SqlDataReader
 
-    Dim db As dbUtil 'access to db functions
-    Dim rsData As SqlDataReader
+        db = New dbUtil()
+        rsData = db.GetDataReader("SELECT QwkMsgID, QwkMsg FROM QwkMsg WITH (NOLOCK) ORDER BY QwkMsg ASC")
 
-    db = New dbUtil()
-    rsData = db.GetDataReader("SELECT QwkMsgID, QwkMsg FROM QwkMsg WITH (NOLOCK) ORDER BY QwkMsg ASC")
+        Do While rsData.Read()
+            QwkMessage.Items.Add(New ListItem(rsData("QwkMsg").ToString(), rsData("QwkMsgID").ToString()))
+            'quickMessage.Items.Add(New ListItem(rsData("QwkMsg").ToString(), rsData("QwkMsgID").ToString()))
+        Loop
 
-    Do While rsData.Read()
-      QwkMessage.Items.Add(New ListItem(rsData("QwkMsg").ToString(), rsData("QwkMsgID").ToString()))
-      'quickMessage.Items.Add(New ListItem(rsData("QwkMsg").ToString(), rsData("QwkMsgID").ToString()))
-    Loop
+    End Sub
 
-  End Sub
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Function GetClient(id As String) As Boolean
 
-  ''' <summary>
-  ''' 
-  ''' </summary>
-  ''' <remarks></remarks>
-  Private Function GetClient(id As String) As Boolean
+        Dim db As dbUtil 'access to db functions
+        Dim rsData As SqlDataReader
 
-    Dim db As dbUtil 'access to db functions
-    Dim rsData As SqlDataReader
+        db = New dbUtil()
+        'rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact, ClientType, ClientAnswer, ClientData FROM ClientUpdate WITH (NOLOCK) WHERE CustID = " + clientId)
+        rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact, ClientType, ClientAnswer, ClientData FROM CompanyInfo WITH (NOLOCK) WHERE CustID = " + id)
 
-    db = New dbUtil()
-    'rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact, ClientType, ClientAnswer, ClientData FROM ClientUpdate WITH (NOLOCK) WHERE CustID = " + clientId)
-    rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact, ClientType, ClientAnswer, ClientData FROM CompanyInfo WITH (NOLOCK) WHERE CustID = " + id)
+        Do While rsData.Read()
+            clientMessageId.InnerHtml = rsData("CustID").ToString()
+            clientName.InnerText = rsData("CompanyName")
+            clientGreeting = rsData("ClientAnswer")
+            clientMainInfo.Value = db.ClearNull(rsData("ClientData"))
+        Loop
 
-    Do While rsData.Read()
-      clientMessageId.InnerHtml = rsData("CustID").ToString()
-      clientName.InnerText = rsData("CompanyName")
-      clientGreeting = rsData("ClientAnswer")
-      clientMainInfo.Value = db.ClearNull(rsData("ClientData"))
-    Loop
+        Return True
 
-    Return True
+    End Function
 
-  End Function
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="msgId"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function GetMessage(msgId As Integer) As Boolean
 
-  ''' <summary>
-  ''' 
-  ''' </summary>
-  ''' <param name="msgId"></param>
-  ''' <returns></returns>
-  ''' <remarks></remarks>
-  Private Function GetMessage(msgId As Integer) As Boolean
+        Dim db As dbUtil 'access to db functions
+        Dim rsData As SqlDataReader
 
-    Dim db As dbUtil 'access to db functions
-    Dim rsData As SqlDataReader
+        db = New dbUtil()
+        rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact, ClientType, ClientAnswer, ClientData FROM ClientUpdate WITH (NOLOCK) WHERE CustID = " + clientId)
 
-    db = New dbUtil()
-    rsData = db.GetDataReader("SELECT CustID, CompanyName, Contact, ClientType, ClientAnswer, ClientData FROM ClientUpdate WITH (NOLOCK) WHERE CustID = " + clientId)
+        Do While rsData.Read()
 
-    Do While rsData.Read()
+        Loop
 
-    Loop
+        Return True
 
-    Return True
+    End Function
 
-  End Function
+    ''' <summary>
+    ''' Event Handler on Submit Message button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Protected Sub submitMessage_Click(sender As Object, e As EventArgs) Handles submitMessage.Click
 
-  ''' <summary>
-  ''' Event Handler on Submit Message button
-  ''' </summary>
-  ''' <param name="sender"></param>
-  ''' <param name="e"></param>
-  ''' <remarks></remarks>
-  Protected Sub submitMessage_Click(sender As Object, e As EventArgs) Handles submitMessage.Click
+        Dim returnedID As Integer
+        Dim SQL As New StringBuilder()
+        Dim test As String = ""
+        Dim db As dbUtil 'access to db functions
+        db = New dbUtil()
 
-    Dim returnedID As Integer
-    Dim SQL As New StringBuilder()
-    Dim test As String = ""
-    Dim db As dbUtil 'access to db functions
-    db = New dbUtil()
+        SQL.Append("INSERT INTO [dbo].[Msg] ([MsgDateTime],[MsgCustID],[MsgDate],[MsgTime],[MsgTo],[MsgFrom],[MsgBusiness],[MsgPhone],[MsgExt],[MsgAltPhone],[MsgQwkMsgs],[MsgMessage],[MsgOperatorNotes],[MsgHoldMsg],[MsgDelDate],[MsgDelTime],[MsgDeliver],[MsgOnCall],[MsgProcedure])")
+        SQL.Append(" VALUES ")
+        SQL.Append("('" & DateTime.Now & "',") 'MsgDateTime
+        SQL.Append("'" & Convert.ToInt32(clientMessageId.InnerHtml) & "',") 'MsgCustID
+        SQL.Append("'" & Date.Now & "',") 'MsgDate
+        SQL.Append("'" & FormatDateTime(DateTime.Now, DateFormat.LongTime) & "',") 'MsgTime
+        SQL.Append("'" & MsgTo.Text & "',") 'MsgTo
+        SQL.Append("'" & MsgFrom.Text & "',") 'MsgFrom
+        SQL.Append("'" & String.Empty & "',") 'MsgBusiness
+        SQL.Append("'" & nMsgPhone.Text & "',") 'MsgPhone
+        SQL.Append("'" & nMsgPhoneX.Text & "',") 'MsgExt
+        SQL.Append("'" & nMsgAlt.Text & "',") 'MsgAltPhone
+        SQL.Append("'" & QwkMessage.SelectedItem.Text & "',") 'MsgQwkMsgs
+        SQL.Append("'" & Message.Text & "',") 'MsgMessage
+        SQL.Append("'" & Notes.Text & "',") 'MsgOperatorNotes
+        SQL.Append("1,") 'MsgHoldMsg
+        SQL.Append("NULL,") 'MsgDelDate
+        SQL.Append("NULL,") 'MsgDelTime
+        SQL.Append("0,") 'MsgDeliver
+        SQL.Append("NULL,") 'MsgOnCall
+        SQL.Append("NULL)") 'MsgProcedure
 
-    SQL.Append("INSERT INTO [dbo].[Msg] ([MsgDateTime],[MsgCustID],[MsgDate],[MsgTime],[MsgTo],[MsgFrom],[MsgBusiness],[MsgPhone],[MsgExt],[MsgAltPhone],[MsgQwkMsgs],[MsgMessage],[MsgOperatorNotes],[MsgHoldMsg],[MsgDelDate],[MsgDelTime],[MsgDeliver],[MsgOnCall],[MsgProcedure])")
-    SQL.Append(" VALUES ")
-    SQL.Append("('" & DateTime.Now & "',") 'MsgDateTime
-    SQL.Append("'" & Convert.ToInt32(clientMessageId.InnerHtml) & "',") 'MsgCustID
-    SQL.Append("'" & Date.Now & "',") 'MsgDate
-    SQL.Append("'" & FormatDateTime(DateTime.Now, DateFormat.LongTime) & "',") 'MsgTime
-    SQL.Append("'" & MsgTo.Text & "',") 'MsgTo
-    SQL.Append("'" & MsgFrom.Text & "',") 'MsgFrom
-    SQL.Append("'" & String.Empty & "',") 'MsgBusiness
-    SQL.Append("'" & nMsgPhone.Text & "',") 'MsgPhone
-    SQL.Append("'" & nMsgPhoneX.Text & "',") 'MsgExt
-    SQL.Append("'" & nMsgAlt.Text & "',") 'MsgAltPhone
-    SQL.Append("'" & QwkMessage.SelectedItem.Text & "',") 'MsgQwkMsgs
-    SQL.Append("'" & Message.Text & "',") 'MsgMessage
-    SQL.Append("'" & Notes.Text & "',") 'MsgOperatorNotes
-    SQL.Append("1,") 'MsgHoldMsg
-    SQL.Append("NULL,") 'MsgDelDate
-    SQL.Append("NULL,") 'MsgDelTime
-    SQL.Append("0,") 'MsgDeliver
-    SQL.Append("NULL,") 'MsgOnCall
-    SQL.Append("NULL)") 'MsgProcedure
 
-   
-    returnedID = db.GetID(SQL.ToString())
-    Response.Redirect("Main.aspx")
+        returnedID = db.GetID(SQL.ToString())
+        Response.Redirect("Main.aspx")
 
-  End Sub
+    End Sub
 
   ''' <summary>
   ''' Save New Message
