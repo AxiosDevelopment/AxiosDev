aed5ef6ccc863536e4386d73aa2e287d0fade913
kengood1 <kengoodman25@gmail.com>
2014-09-11 14:02:52 -0700

    Coded Update and Insert functions for Companies

--- DAL/CompanyDA.vb
+++ DAL/CompanyDA.vb
@@ -49,11 +49,116 @@ Public Class CompanyDA
 
   End Function
 
+  ''' <summary>
+  ''' Insert Company
+  ''' </summary>
+  ''' <param name="c"></param>
+  ''' <returns></returns>
+  ''' <remarks></remarks>
   Public Function InsertCompany(c As Company) As Integer Implements ICompanyDA.InsertCompany
+
+    Dim SQL As New StringBuilder()
+    Dim db As dbUtil 'access to db functions
+    db = New dbUtil()
+
+    SQL.Append("NSERT INTO [dbo].[COMPANY]([CompanyNumber],[CompanyName],[CompanyTypeID],[CompanyPhoneAnswer],[CompanyPhoneForwarded],[CompanyMainTelephone],[CompanyMainTelephone2nd],[CompanyMainTelephone3rd],[CompanyBacklines]")
+    SQL.Append(",[CompanyFax],[CompanyFax2],[CompanyEmail],[CompanyMailingAddress],[CompanyMailingAddress2],[CompanyMailingCity],[CompanyMailingState],[CompanyMailingZip],[CompanyMailingCountry],[CompanyBillingAddress]")
+    SQL.Append(",[CompanyBillingAddress2],[CompanyBillingCity],[CompanyBillingState],[CompanyBillingZip],[CompanyBillingCountry],[CompanyBillBaseRate],[CompanyCallOutCharge],[CompanyCallOutLimit],[CompanyMsgLimit]")
+    SQL.Append(",[CompanyFirstCallProc],[CompanyMsgProc],[CompanyInstructionSheet],[CompanyClActive],[CompanyMenuNotes],[CompanyTransferNo],[CompanyEmployeeList],[CompanyAdditionalNotes])")
+    SQL.Append(" VALUES ")
+    SQL.Append("(" & c.Number & ",")
+    SQL.Append("'" & c.Name & "',")
+    SQL.Append("" & c.TypeID & ",")
+    SQL.Append("'" & c.PhoneAnswer & "',")
+    SQL.Append("'" & c.PhoneForwarded & "',")
+    SQL.Append("'" & c.MainTelephone & "',")
+    SQL.Append("'" & c.MainTelephone2nd & "',")
+    SQL.Append("'" & c.MainTelephone3rd & "',")
+    SQL.Append("'" & c.Backlines & "',")
+    SQL.Append("'" & c.Fax & "',")
+    SQL.Append("'" & c.Fax2 & "',")
+    SQL.Append("'" & c.Email & "',")
+    SQL.Append("'" & c.MailingAddress & "',")
+    SQL.Append("'" & c.MailingAddress2 & "',")
+    SQL.Append("'" & c.MailingCity & "',")
+    SQL.Append("'" & c.MailingState & "',")
+    SQL.Append("'" & c.MailingZip & "',")
+    SQL.Append("'" & c.MailingCountry & "',")
+    SQL.Append("'" & c.BillingAddress & "',")
+    SQL.Append("'" & c.BillingAddress2 & "',")
+    SQL.Append("'" & c.BillingCity & "',")
+    SQL.Append("'" & c.BillingState & "',")
+    SQL.Append("'" & c.BillingZip & "',")
+    SQL.Append("'" & c.BillingCountry & "',")
+    SQL.Append("" & c.BillBaseRate & ",")
+    SQL.Append("" & c.CallOutCharge & ",")
+    SQL.Append("" & c.CallOutLimit & ",")
+    SQL.Append("" & c.MsgLimit & ",")
+    SQL.Append("'" & c.FirstCallProc & "',")
+    SQL.Append("'" & c.MsgProc & "',")
+    SQL.Append("'" & c.InstructionSheet & "',")
+    SQL.Append("" & c.CIActive & ",")
+    SQL.Append("'" & c.MenuNotes & "',")
+    SQL.Append("'" & c.TransferNo & "',")
+    SQL.Append("'" & c.EmployeeList & "',")
+    SQL.Append("'" & c.AdditionalNotes & "')")
+
+    Return db.GetID(SQL.ToString())
 
   End Function
 
+  ''' <summary>
+  ''' Update Company
+  ''' </summary>
+  ''' <param name="c"></param>
+  ''' <returns></returns>
+  ''' <remarks></remarks>
   Public Function UpdateCompany(c As Company) As Integer Implements ICompanyDA.UpdateCompany
+
+    Dim SQL As New StringBuilder()
+    Dim db As dbUtil 'access to db functions
+    db = New dbUtil()
+
+    SQL.Append("UPDATE COMPANY SET ")
+    SQL.Append("[CompanyNumber] = " & c.CompanyID & ",")
+    SQL.Append("[CompanyName] = '" & c.Name & "',")
+    SQL.Append("[CompanyTypeID] = " & c.TypeID & ",")
+    SQL.Append("[CompanyPhoneAnswer] = '" & c.PhoneAnswer & "',")
+    SQL.Append("[CompanyPhoneForwarded] = '" & c.PhoneForwarded & "',")
+    SQL.Append("[CompanyMainTelephone] = '" & c.MainTelephone & "',")
+    SQL.Append("[CompanyMainTelephone2nd] = '" & c.MainTelephone2nd & "',")
+    SQL.Append("[CompanyMainTelephone3rd] = '" & c.MainTelephone3rd & "',")
+    SQL.Append("[CompanyBacklines] = '" & c.Backlines & "',")
+    SQL.Append("[CompanyFax] = '" & c.Fax & "',")
+    SQL.Append("[CompanyFax2] = '" & c.Fax2 & "',")
+    SQL.Append("[CompanyEmail] = '" & c.Email & "',")
+    SQL.Append("[CompanyMailingAddress] = '" & c.MailingAddress & "',")
+    SQL.Append("[CompanyMailingAddress2] = '" & c.MailingAddress2 & "',")
+    SQL.Append("[CompanyMailingCity] = '" & c.MailingCity & "',")
+    SQL.Append("[CompanyMailingState] = '" & c.MailingState & "',")
+    SQL.Append("[CompanyMailingZip] = '" & c.MailingZip & "',")
+    SQL.Append("[CompanyMailingCountry] = '" & c.MailingCountry & "',")
+    SQL.Append("[CompanyBillingAddress] = '" & c.BillingAddress & "',")
+    SQL.Append("[CompanyBillingAddress2] = '" & c.BillingAddress2 & "',")
+    SQL.Append("[CompanyBillingCity] = '" & c.BillingCity & "',")
+    SQL.Append("[CompanyBillingState] = '" & c.BillingState & "',")
+    SQL.Append("[CompanyBillingZip] = '" & c.BillingZip & "',")
+    SQL.Append("[CompanyBillingCountry] = '" & c.BillingCountry & "',")
+    SQL.Append("[CompanyBillBaseRate] = " & c.BillBaseRate & ",")
+    SQL.Append("[CompanyCallOutCharge] = " & c.CallOutCharge & ",")
+    SQL.Append("[CompanyCallOutLimit] = " & c.CallOutLimit & ",")
+    SQL.Append("[CompanyMsgLimit] = " & c.MsgLimit & ",")
+    SQL.Append("[CompanyFirstCallProc] = '" & c.FirstCallProc & "',")
+    SQL.Append("[CompanyMsgProc] = '" & c.MsgProc & "',")
+    SQL.Append("[CompanyInstructionSheet] = '" & c.InstructionSheet & "',")
+    SQL.Append("[CompanyClActive] = " & c.CIActive & ",")
+    SQL.Append("[CompanyMenuNotes] = '" & c.MenuNotes & "',")
+    SQL.Append("[CompanyTransferNo] = '" & c.TransferNo & "',")
+    SQL.Append("[CompanyEmployeeList] = '" & c.EmployeeList & "',")
+    SQL.Append("[CompanyAdditionalNotes] = '" & c.AdditionalNotes & "'")
+    SQL.Append("WHERE CompanyID = " & c.CompanyID)
+
+    Return db.GetID(SQL.ToString())
 
   End Function
 
@@ -65,7 +170,6 @@ Public Class CompanyDA
   ''' <remarks></remarks>
   Public Function UpdateCompanyAdditionalNotes(c As Company) As Integer Implements ICompanyDA.UpdateCompanyAdditionalNotes
 
-    Dim returnedID As Integer
     Dim SQL As New StringBuilder()
     Dim db As dbUtil 'access to db functions
     db = New dbUtil()
@@ -74,7 +178,7 @@ Public Class CompanyDA
     SQL.Append("CompanyAdditionalNotes = " & If(Not String.IsNullOrEmpty(c.AdditionalNotes), "'" & c.AdditionalNotes & "' ", "NULL "))
     SQL.Append("WHERE CompanyID = " & c.CompanyID)
 
-    returnedID = db.GetID(SQL.ToString())
+    Return db.GetID(SQL.ToString())
 
   End Function
 
@@ -86,7 +190,6 @@ Public Class CompanyDA
   ''' <remarks></remarks>
   Public Function UpdateCompanyInfo(c As Company) As Integer Implements ICompanyDA.UpdateCompanyInfo
 
-    Dim returnedID As Integer
     Dim SQL As New StringBuilder()
     Dim db As dbUtil 'access to db functions
     db = New dbUtil()
@@ -95,7 +198,7 @@ Public Class CompanyDA
     SQL.Append("CompanyInformation = " & If(Not String.IsNullOrEmpty(c.ClientInfo), "'" & c.ClientInfo & "' ", "NULL "))
     SQL.Append("WHERE CompanyID = " & c.CompanyID)
 
-    returnedID = db.GetID(SQL.ToString())
+    Return db.GetID(SQL.ToString())
 
   End Function
 
